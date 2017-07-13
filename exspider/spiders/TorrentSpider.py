5e55ad9be2a1baa2a9a94c39e3f70e5c708c82c8
yangfc <uestc2803305011@gmail.com>
2017-07-14 00:51:53 +0800

    still initial

--- exspider/spiders/TorrentSpider.py
+++ exspider/spiders/TorrentSpider.py
@@ -1,15 +1,16 @@
 import scrapy
+from scrapy.shell import inspect_response
 
 
 class TorrentSpider(scrapy.Spider):
     name = "torrent"
     user_agent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/59.0.3071.86 Safari/537.36"
 
-    def __init__(self, user, rule):
-        self.username = user['username']
-        self.password = user['password']
-        self.rule = rule
-        self.page = 1
+    # def __init__(self, user, rule):
+        # self.username = user['username']
+        # self.password = user['password']
+        # self.rule = rule
+        # self.page = 1
 
     def start_requests(self):
         return [scrapy.FormRequest(
@@ -30,22 +31,42 @@ class TorrentSpider(scrapy.Spider):
         )]
 
     def second_login(self, response):
+        # return [scrapy.Request(
+        #     "https://exhentai.org/?page={0}&f_doujinshi={1}&f_manga={2}&f_artistcg={3}&f_gamecg={4}&f_western={5}&f_non-h={6}&f_imageset={7}&f_cosplay={8}&f_asianporn={9}&f_misc={10}&f_search={11}&f_apply=Apply+Filter&inline_set=dm_t"
+        #     .format(self.rule['start_page'] - 1,
+        #             self.rule['doujinshi'],
+        #             self.rule['manga'],
+        #             self.rule['artist_cg'],
+        #             self.rule['game_cg'],
+        #             self.rule['western'],
+        #             self.rule['non_h'],
+        #             self.rule['image_set'],
+        #             self.rule['cosplay'],
+        #             self.rule['asian_porn'],
+        #             self.rule['misc'],
+        #             self.rule['keyword']),
+        #     meta={'cookiejar': response.meta['cookiejar']}
+        # )]
         return [scrapy.Request(
-            "https://exhentai.org/?page={0}&f_doujinshi={1}&f_manga={2}&f_artistcg={3}&f_gamecg={4}&f_western={5}&f_non-h={6}&f_imageset={7}&f_cosplay={8}&f_asianporn={9}&f_misc={10}&f_search={11}&f_apply=Apply+Filter&inline_set=dm_t"
-            .format(self.rule['start_page'] - 1,
-                    self.rule['doujinshi'],
-                    self.rule['manga'],
-                    self.rule['artist_cg'],
-                    self.rule['game_cg'],
-                    self.rule['western'],
-                    self.rule['non_h'],
-                    self.rule['image_set'],
-                    self.rule['cosplay'],
-                    self.rule['asian_porn'],
-                    self.rule['misc'],
-                    self.rule['keyword']),
+            "https://exhentai.org/?page={0}&f_doujinshi={1}&f_manga={2}&f_artistcg={3}&f_gamecg={4}&f_western={5}&f_non-h={6}&f_imageset={7}&f_cosplay={8}&f_asianporn={9}&f_misc={10}&f_search={11}&f_apply=Apply+Filter&inline_set=dm_l"
+            .format(1,
+                    'on',
+                    'on',
+                    'on',
+                    'on',
+                    'on',
+                    'on',
+                    'on',
+                    'on',
+                    'on',
+                    'on',
+                    'chinese'),
             meta={'cookiejar': response.meta['cookiejar']}
         )]
 
     def parse(self, response):
-        articles = response.css()
+        inspect_response(response, self)
+        articles = response.css('.it5 a::attr(href)').extract()
+
+        for article in articles:
+
